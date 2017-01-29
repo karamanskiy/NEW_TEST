59a22b188f00f634f8421bf5e5a2d827c99cd701
John <karamanskiy@mail.ru>
2017-01-29 11:22:03 +0200

    2nd_commit

---
+++ src/desktop/js/main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+$(function(){
+
+	$(document).ready(function(){
+		
+		$('input, textarea').focus(function(){
+			$(this).data('placeholder',$(this).attr('placeholder'))
+			$(this).attr('placeholder','');
+		 });
+		 $('input,textarea').blur(function(){
+			$(this).attr('placeholder',$(this).data('placeholder'));
+		 });
+
+
+
+
+
+
+//==========EoF==============
+	});
+});
